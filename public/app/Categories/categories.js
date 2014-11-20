a00f529053e8e1b3958ad114efe0cbc47451a1ae
Seb <dilucci@gmail.com>
2014-11-20 19:46:18 +0100

    Rodet med anchors i categories.html. MERE

--- public/app/Categories/categories.js
+++ public/app/Categories/categories.js
@@ -12,18 +12,6 @@ angular.module('myAppRename.categories', ['ngRoute'])
     }])
     .controller('headerCtrl', ['$anchorScroll', '$location', '$scope', '$http',
         function ($anchorScroll, $location, $scope, $http) {
-            $scope.gotoAnchor = function(x) {
-                var newHash = 'anchor' + x;
-                if ($location.hash() !== newHash) {
-                    // set the $location.hash to `newHash` and
-                    // $anchorScroll will automatically scroll to it
-                    $location.hash('anchor' + x);
-                } else {
-                    // call $anchorScroll() explicitly,
-                    // since $location.hash hasn't changed
-                    $anchorScroll();
-                }
-            };
 
             $http({
                 method: 'GET',
@@ -35,5 +23,45 @@ angular.module('myAppRename.categories', ['ngRoute'])
                 error(function (data, status, headers, config) {
                     $scope.error = data;
                 });
+
+            //$scope.gotoAnchor = function(x) {
+            //        $location.hash('anchorb');
+            //        $anchorScroll();
+            //};
+
+            $scope.gotoAnchor = function(x) {
+                console.log("i gotoAnchor: " + x)
+                var newHash = 'anchor' + x;
+
+                for(var i = 0; i < $scope.categories.length; i++) {
+                    if($scope.categories[i] && $scope.categories[i].charAt(0) === x) {
+                        $location.hash('anchor' + $scope.categories[i])
+                        break;
+                    }
+
+                    //if($scope.categories[i].charAt(0).valueOf() - $scope.categories[i+1].charAt(0).valueOf() === 1)
+                }
+                $anchorScroll();
+
+
+                //$scope.categories.forEach(function(category) {
+                //
+                //})
+
+                //if ($location.hash() !== newHash) {
+                //    // set the $location.hash to `newHash` and
+                //    // $anchorScroll will automatically scroll to it
+                //    $location.hash('anchor' + x);
+                //} else {
+                //    // call $anchorScroll() explicitly,
+                //    // since $location.hash hasn't changed
+                //    $anchorScroll();
+                //}
+            };
+
+            //$scope.anchorPlacement = function(category) {
+            //
+            //}
+
         }
     ]);
